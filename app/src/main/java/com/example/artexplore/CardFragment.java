13ac8f976011b71a72a2a57f039f33f82deb76f0
Bagus Prasetyo <prasetyo.raharja12@gmail.com>
2019-05-16 00:57:52 +0800

    payment activity

--- app/src/main/java/com/example/artexplore/CardFragment.java
+++ app/src/main/java/com/example/artexplore/CardFragment.java
@@ -1,6 +1,7 @@
 package com.example.artexplore;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,10 +9,11 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
-public class CardFragment extends Fragment {
+public class CardFragment extends Fragment  {
 
     private CardView cardView;
 
@@ -33,18 +35,28 @@ public class CardFragment extends Fragment {
 
         cardView = view.findViewById(R.id.cardView);
         cardView.setMaxCardElevation(cardView.getCardElevation() * CardAdapter.MAX_ELEVATION_FACTOR);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),DaerahActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         TextView title =  view.findViewById(R.id.title);
+        Button pesan =  view.findViewById(R.id.pesan);
+
+        pesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
 
         title.setText(String.format("Wisata Placeholder %d",
                 getArguments().getInt("position")  + 1 ));
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getActivity(), "Button in Card " + getArguments().getInt("position")
-//                        + "Clicked!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
         return view;
     }
